1ae76148a42e40df00ad281ba4a178b8a87da634
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-06-29 23:03:39 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_233105) do
+ActiveRecord::Schema[7.0].define(version: 20_230_614_233_105) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "messages", force: :cascade do |t|
-    t.string "greeting"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "content"
+  create_table 'messages', force: :cascade do |t|
+    t.string 'greeting'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'content'
   end
-
 end
